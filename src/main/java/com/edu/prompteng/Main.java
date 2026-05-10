1c8bb8fbfbd2171edb5926900d3aafb708c5caae
victorsalazarsalinas <victorsalazarsalinas@gmail.com>
2026-05-10 14:46:49 -0600

    feat: agregar clase Main demostrando la ejecución polimórfica de prompts

--- src/main/java/com/edu/prompteng/Main.java
+++ src/main/java/com/edu/prompteng/Main.java
@@ -1,17 +1,38 @@
 package com.edu.prompteng;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.edu.prompteng.core.PromptBuilder;
+import com.edu.prompteng.core.PromptStrategy;
+import com.edu.prompteng.strategies.ChainOfThoughtStrategy;
+import com.edu.prompteng.strategies.FewShotStrategy;
+import com.edu.prompteng.strategies.ZeroShotStrategy;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    private PromptStrategy estrategia;
+
+    public void setEstrategia(PromptStrategy estrategia) {
+        this.estrategia = estrategia;
+    }
+
+    public String construirPrompt(PromptBuilder builder) {
+        return estrategia.definirEstructuraPrompt(builder);
+    }
+
+    public static void main(String[] args) {
+        Main agente = new Main();
+
+        System.out.println("=== ZERO-SHOT PROMPTING ===");
+        PromptBuilder builderZero = new PromptBuilder();
+        agente.setEstrategia(new ZeroShotStrategy());
+        System.out.println(agente.construirPrompt(builderZero));
+
+        System.out.println("\n=== FEW-SHOT PROMPTING ===");
+        PromptBuilder builderFew = new PromptBuilder();
+        agente.setEstrategia(new FewShotStrategy());
+        System.out.println(agente.construirPrompt(builderFew));
+
+        System.out.println("\n=== CHAIN-OF-THOUGHT PROMPTING ===");
+        PromptBuilder builderCoT = new PromptBuilder();
+        agente.setEstrategia(new ChainOfThoughtStrategy());
+        System.out.println(agente.construirPrompt(builderCoT));
     }
 }
